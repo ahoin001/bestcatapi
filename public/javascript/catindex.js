6fa2d52f91a112b042df4221473c784e7925487b
Alexander Hoinville <ahoin001@gmail.com>
2020-07-20 17:52:13 -0400

    ready for heroku deployment

---
+++ public/javascript/catindex.js
@@ -0,0 +1,179 @@
+// ****************************************************************
+//? CONNECT TO DOM  & INITIALIZE VARIABLES
+// ****************************************************************
+
+let randomCatContainer = document.querySelector("div.pic-container")
+let randomCatPic = document.querySelector("img")
+
+let lovedCatContainer = document.querySelector("div.loved-container")
+let lovedCatPicsList = document.querySelector(".loved-cat-list")
+
+let voteButtons = document.querySelectorAll(".vote-bttn")
+
+let catImageID;
+let lovedCats = [];
+
+
+const sub_id = `CatLady${Math.floor(Math.random() * 300000)}`;
+console.log(sub_id)
+
+
+// ****************************************************************
+//? API INTERACTIONs
+// ****************************************************************
+
+const fetchCatApi = async (url = "https://api.thecatapi.com/v1/images/search", requestType = "GET", requestBody) => {
+
+    let response;
+
+    if (requestType === "GET") {
+        response = await fetch(
+            url,
+            {
+                method: requestType,
+                headers: {
+                    'x-api-key': '5d07ae96-a25c-474e-94f0-5c6bd5e5a60b',
+                    "Content-Type": "application/json"
+                }
+            })
+    } else {
+
+        response = await fetch(
+            url,
+            {
+                method: requestType,
+                body: requestBody,
+                headers: {
+                    'x-api-key': '5d07ae96-a25c-474e-94f0-5c6bd5e5a60b',
+                    "Content-Type": "application/json"
+                }
+            })
+
+    }
+
+
+    let dataParsed = await response.json();
+
+    // console.log(dataParsed);
+
+    return dataParsed;
+
+}
+
+// ****************************************************************
+//? Utility FUNCTIONS
+// ****************************************************************
+
+const setRandomCat = async () => {
+
+    // Uses CatApi
+    const apiResponse = await fetchCatApi();
+
+    randomCatPic.setAttribute("src", apiResponse[0].url)
+
+    catImageID = apiResponse[0].id;
+
+
+    // Uses practice API
+    // const challengeApi = await fetchCatApi("http://localhost:2000/randomcat")
+    // console.log("FROM MY API: ", challengeApi.catPicUrl)
+
+    // randomCatPic.setAttribute("src", challengeApi.catPicUrl)
+    // catImageID = challengeApi;
+
+}
+
+const postVoteToCatApi = async (requestBody) => {
+
+    let response = await fetchCatApi("https://api.thecatapi.com/v1/votes", "POST", requestBody)
+
+    console.log('Post Request:', response);
+
+}
+
+const lovedCat = async (catImageId) => {
+
+    voteButtons.forEach(bttn => {
+        bttn.disabled = true;
+    })
+
+    let requestBodyVote = JSON.stringify({
+        image_id: catImageId,
+        sub_id,
+        value: 1
+    })
+
+    // console.log('Your cat vote: ', requestBodyVote)
+
+    await postVoteToCatApi(requestBodyVote)
+
+    await setRandomCat()
+
+    await getVotedCats();
+
+    voteButtons.forEach(bttn => {
+        bttn.disabled = false;
+    })
+
+
+}
+
+const nopedCat = async (catImageId) => {
+
+    voteButtons.forEach(bttn => {
+        bttn.disabled = true;
+    })
+
+    let requestBodyVote = {
+        image_id: catImageId,
+        sub_id,
+        value: 0
+    }
+
+    await postVoteToCatApi(JSON.stringify(requestBodyVote))
+
+    await setRandomCat()
+
+    voteButtons.forEach(bttn => {
+        bttn.disabled = false;
+    })
+
+}
+
+const getVotedCats = async () => {
+
+    const catsThatWereVoted = await fetchCatApi(`https://api.thecatapi.com/v1/votes?sub_id=${sub_id}`)
+    // const catsThatWereVoted = await fetchCatApi(`https://api.thecatapi.com/v1/votes`)
+
+    console.log(`Cats that were voted: `, catsThatWereVoted)
+
+    const recentCat = catsThatWereVoted.pop()
+
+    if (recentCat.value === 1) {
+
+        const catID = recentCat.image_id;
+
+        const catImageInfo = await fetchCatApi(`https://api.thecatapi.com/v1/images/${catID}`)
+
+        // console.log('******', catImageInfo)
+
+        let lovedCatImg = document.createElement("img");
+
+        lovedCatImg.setAttribute("src", catImageInfo.url)
+        lovedCatImg.setAttribute("class", "fav-cat")
+
+        lovedCatPicsList.appendChild(lovedCatImg)
+
+    } else {
+        console.log(`Cat not loved :( : ${recentCat} `)
+    }
+
+
+}
+
+
+// ****************************************************************
+//? FUNCTIONS In action 
+// ****************************************************************
+
+setRandomCat()
---
+++ src/app.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const cors = require('cors')
+
+const express = require('express')
+
+const app = express();
+
+console.log(path.join(__dirname, '../public/'))
+const publicDirectory = path.join(__dirname,'../public')
+
+// Allow Clients to access api
+app.use(cors())
+
+app.use(express.static(publicDirectory))
+
+// ? Create sever listening for requests on PORT
+app.listen(process.env.PORT || 2000, () => {
+    if (process.env.PORT) {
+        console.log(`App listening on port ${process.env.PORT}!`);
+    } else {
+        console.log(`App listening on port! 2000`);
+    }
+});
+
+// ? Returns random Cat Pic 
+app.get('/randomcat', (req, res) => {
+
+    const randomPics = [
+
+        "https://cdn2.thecatapi.com/images/lm.jpg",
+        "https://cdn2.thecatapi.com/images/e27.jpg",
+        "https://cdn2.thecatapi.com/images/MTYyNjA5Nw.jpg",
+        "https://cdn2.thecatapi.com/images/ahm.jpg",
+        "https://cdn2.thecatapi.com/images/ahm.jpg",
+        "https://cdn2.thecatapi.com/images/btb.jpg",
+        "https://cdn2.thecatapi.com/images/aec.jpg",
+        "https://cdn2.thecatapi.com/images/dfq.jpg",
+        "https://cdn2.thecatapi.com/images/MTkyOTk5Mw.jpg",
+        "https://cdn2.thecatapi.com/images/3ju.jpg",
+        "https://cdn2.thecatapi.com/images/1tb.jpg",
+        "https://cdn2.thecatapi.com/images/MTY4NjQ4OA.jpg",
+        "https://cdn2.thecatapi.com/images/5j7.jpg",
+        "https://cdn2.thecatapi.com/images/Vq1TbtBm0.jpg",
+        "https://cdn2.thecatapi.com/images/b46.jpg"
+
+    ];
+
+    let randomPicIndex = Math.floor(Math.random() * randomPics.length);
+
+    res.json({
+        catPicUrl: randomPics[randomPicIndex]
+    })
+
+});
