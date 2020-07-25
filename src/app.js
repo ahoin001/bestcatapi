d55210a4c14d92dda094465e07ac40598b699962
Alexander Hoinville <ahoin001@gmail.com>
2020-07-24 22:34:18 -0400

    Use router module from express Split routes for cat http requests to their own file

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 const express = require('express')
 
+const catRoutes = require('../routes/cat-routes');
+
 const app = express();
 
 console.log(path.join(__dirname, '../public/'))
@@ -27,45 +29,4 @@ app.listen(process.env.PORT || 2000, () => {
     }
 });
 
-const nameList =[];
-
-// ? Returns random Cat Pic 
-app.get('/randomcat', (req, res) => {
-
-    const randomPics = [
-
-        "https://cdn2.thecatapi.com/images/lm.jpg",
-        "https://cdn2.thecatapi.com/images/e27.jpg",
-        "https://cdn2.thecatapi.com/images/MTYyNjA5Nw.jpg",
-        "https://cdn2.thecatapi.com/images/ahm.jpg",
-        "https://cdn2.thecatapi.com/images/ahm.jpg",
-        "https://cdn2.thecatapi.com/images/btb.jpg",
-        "https://cdn2.thecatapi.com/images/aec.jpg",
-        "https://cdn2.thecatapi.com/images/dfq.jpg",
-        "https://cdn2.thecatapi.com/images/MTkyOTk5Mw.jpg",
-        "https://cdn2.thecatapi.com/images/3ju.jpg",
-        "https://cdn2.thecatapi.com/images/1tb.jpg",
-        "https://cdn2.thecatapi.com/images/MTY4NjQ4OA.jpg",
-        "https://cdn2.thecatapi.com/images/5j7.jpg",
-        "https://cdn2.thecatapi.com/images/Vq1TbtBm0.jpg",
-        "https://cdn2.thecatapi.com/images/b46.jpg"
-
-    ];
-
-    let randomPicIndex = Math.floor(Math.random() * randomPics.length);
-
-    res.json({
-        catPicUrl: randomPics[randomPicIndex]
-    })
-
-});
-
-app.post('/catname', (req, res) => {
-    
-    // console.log('Request Body: ',req.body)
-
-    res.json({
-        status: 'Success Addition!'
-    })
-
-});
+app.use('/', catRoutes);
