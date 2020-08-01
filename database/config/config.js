60ca3ec52392f02691db122f6784da4460f25547
Alexander Hoinville <ahoin001@gmail.com>
2020-08-01 16:35:04 -0400

    Change env configs for sequelize

--- database/config/config.js
+++ database/config/config.js
@@ -15,6 +15,10 @@ module.exports = {
         "dialect": "postgres"
     },
     "production": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
         "use_env_variable": process.env.DATABASE_URL,
         "dialect": 'postgres'
     }
--- database/models/index.js
+++ database/models/index.js
@@ -7,7 +7,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 console.log(`@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-: nodenev `, env)
+: ENV `, env)
 
 
 const config = require(__dirname + '/../config/config.json')[env];
@@ -15,7 +15,7 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 console.log(`@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-: BAM BAM `, config)
+: Config Object `, config)
 
 let sequelize;
 if (config.use_env_variable) {
