e2bfd06c37deef6e8cdca2c0dd694d8e6b89ba0e
Alexander Hoinville <ahoin001@gmail.com>
2020-08-01 15:50:59 -0400

    Adhusted config.js for deployment

--- database/config/config.js
+++ database/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); 
+require('dotenv').config();
 module.exports = {
     "development": {
         "username": process.env.DB_USERNAME,
@@ -15,10 +15,9 @@ module.exports = {
         "dialect": "postgres"
     },
     "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
-        "host": "127.0.0.1",
-        "dialect": "postgres"
+        "use_env_variable": process.env.DB_DATABASE,
+        "dialect": 'postgres'
     }
 };
+
+// postgresql-cylindrical-57741 as DATABASE_URL
