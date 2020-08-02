8f22af3f89c58f6a98fa76bb8da091d7b5bbd3c6
Alexander Hoinville <ahoin001@gmail.com>
2020-08-01 21:37:46 -0400

    Attempt index change

--- database/config/config.js
+++ database/config/config.js
@@ -15,7 +15,8 @@ module.exports = {
         "dialect": "postgres"
     },
     "production": {
-        "use_env_variable": process.env.DB_DATABASE_URL,
+        // "use_env_variable": process.env.DB_DATABASE_URL,
+        "use_env_variable": "postgres://pxdpjxsxmzbebl:e2bbda48ceaa829326c7fcf23e53c5ece849d10d1794c7e0eb804c67d2ff3be2@ec2-54-91-178-234.compute-1.amazonaws.com:5432/d10ghdm8v5vegb",
         "dialect": "postgres"
     }
 };
