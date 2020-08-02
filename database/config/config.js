523447202376a73af124178f9c6672c6773c55e5
Alexander Hoinville <ahoin001@gmail.com>
2020-08-01 20:02:58 -0400

    Adjusted config file to prvide database url from heroku

--- database/config/config.js
+++ database/config/config.js
@@ -15,9 +15,10 @@ module.exports = {
         "dialect": "postgres"
     },
     "production": {
-        "username": "root",
-        "password": null,
-        "database": "database_production",
+        "use_env_variable": process.env.DB_DATABASE_URL,
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
         "host": "127.0.0.1",
         "dialect": "postgres"
     }
