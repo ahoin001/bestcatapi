1b911a7013cf5b68c0a1cf0e1521300aab94b84c
Alexander Hoinville <ahoin001@gmail.com>
2020-08-02 15:47:21 -0400

    Make sure no consecutive cats are selected

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -165,10 +165,18 @@ const setRandomCatFromDatabase = async () => {
 
             const theListOfCats = objectReturnedFromQueryHoldingCatTable.cats
 
-            let randomCatIndex = Math.floor(Math.random() * (theListOfCats.length - 1));
+
+            let randomCatIndex = Math.floor(Math.random() * (theListOfCats.length - 1) + 1);
+
+            while (catImageID === theListOfCats[randomCatIndex].catId) {
+
+                console.log(`WHOA! Same cat , we'll roll again!`)
+                randomCatIndex = Math.floor(Math.random() * (theListOfCats.length - 1) + 1);
+
+            }
 
             console.log('INDEX OF RANDOM: ', randomCatIndex)
-
+      
             const { catId, catImageUrl, loved } = theListOfCats[randomCatIndex]
 
             randomCatPic.setAttribute("src", catImageUrl)
@@ -273,7 +281,6 @@ const unLoveCatVoteToDatabase = async () => {
     try {
 
         const responseAfterUpdatingCat = await fetchCatDatabaseApi(
-            // "http://localhost:2000/cats",
             `${apiUrl}/cats`,
             "PATCH",
             requestBodyLovedCat)
@@ -302,7 +309,6 @@ const deleteCatFromDatabase = async () => {
     try {
 
         const DeletedCatFromDatabaseResponse = await fetchCatDatabaseApi(
-            // `http://localhost:2000/cats/${idOfCatToDelete}`,
             `${apiUrl}/cats/${idOfCatToDelete}`,
             "DELETE",
         )
@@ -335,14 +341,14 @@ const getVotedCatsFromDatabase = async () => {
         lovedCatPicsList.removeChild(lovedCatPicsList.firstChild);
     }
 
-    let lovedCats; 
+    let lovedCats;
 
     if (lovedCatsFromDatabaseResponse.lovedCats) {
-        
+
         lovedCats = lovedCatsFromDatabaseResponse.lovedCats;
 
     } else {
-         lovedCats =[]; 
+        lovedCats = [];
     }
 
     lovedCats.forEach((cat) => {
